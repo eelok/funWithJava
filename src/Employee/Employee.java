446fc032676f266bda3be5ace3a1b643715bfab3
eelok <eelok.ru>
2018-05-27 14:31:36 +0200

    add class employee

---
+++ src/Employee/Employee.java
@@ -0,0 +1,57 @@
+package Employee;
+
+public class Employee {
+	
+	private int id;
+	private int salary;
+	private String firstName;
+	private String lastName;
+	
+	public Employee(int id, String firstName, String lastName, int salary) {
+		this.id = id;
+		this.salary = salary;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	public String getFullName() {
+		return this.firstName + " "+ this.lastName;
+	}
+	
+	public int getSalary() {
+		return this.salary;
+	}
+	
+	public int getAnnualSalary() {
+		return this.salary * 12;
+	}
+	
+	public int raiseSalary(int percent) {
+		return (this.salary * percent/100) + this.salary;
+	}
+	
+	public String toString() {
+		return "Employee ID"+
+				this.id + 
+				", First name: " +
+				this.firstName + 
+				", Last Name: " + 
+				this.lastName+ 
+				", salary pro month: " +
+				this.salary +
+				", salary pro year: " +
+				getAnnualSalary();
+	}
+
+}
---
+++ src/Employee/EmployeeTest.java
@@ -0,0 +1,20 @@
+package Employee;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		Employee person1 = new Employee(1234, "Nick", "Veerov", 10000);
+		person1.getID();
+		person1.getFirstName();
+		person1.getLastName();
+		person1.getFullName();
+		person1.getSalary();
+		person1.getAnnualSalary();
+		person1.raiseSalary(10);
+		person1.toString();
+		System.out.println(person1.toString());
+		
+		
+	}
+
+}
